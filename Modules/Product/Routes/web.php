4a586fe92238760624e63fd5bd4d14d0bd2c24d1
ajilesh p p <ajleshpp2@gmail.com>
2022-06-02 17:52:36 +0400

    module product route change

--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// use Illuminate\Support\Facades\Route;
+ use Illuminate\Support\Facades\Route;
 
 Route::prefix('product')->group(function() {
     Route::middleware(['auth:admin','preventBackHistory'])->group(function(){
